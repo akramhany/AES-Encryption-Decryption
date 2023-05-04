0ebf631fab40e6c75d11bf6db14a7a748d7e7265
Ahmed Aladdin <racerahmed40@gmail.com>
2023-05-04 18:33:28 +0300

    created inv_cipher

---
+++ Decryption/inv_cipher.v
@@ -0,0 +1,52 @@
+
+`include "../key_expansion.v"
+`include "../add_round_key.v"
+`include "inv_sub_bytes.v"
+`include "inv_shift_rows.v"
+`include "inv_mix_columns.v"
+
+module inv_cipher #(
+    parameter NK = 4,
+    parameter NR = 10
+    ) 
+(
+    i_data,
+    i_key,
+    o_data
+);
+input wire [(32 * NK) - 1 : 0]i_key;
+input wire [127:0]i_data;
+output wire [127:0]o_data;
+
+wire [(4 * (NR + 1) * 32) - 1 : 0] expanded_key;
+key_expansion #(NK, NR) expand(.i_cypher_key(i_key), .o_expanded_key(expanded_key));
+
+//128-bit NR and NR-1 long arrays to organize data transmission between the instantiated objects
+wire [127:0]temp_state[0:NR];
+wire [127:0]sub_out[0:NR-1];
+wire [127:0]shift_out[0:NR-1];
+wire [127:0]round_out[0:NR-1];
+
+add_round_key add(.i_state(i_data), .i_key(expanded_key[127 :0]), .o_state(temp_state[0][127:0]));
+
+genvar i;
+generate
+    for(i = 1; i < NR ; i = i + 1) begin
+        inv_shift_rows shift(.i_state(temp_state[i-1][127:0]), .o_state(shift_out[i-1][127:0]));
+        inv_sub_bytes sub(.i_initial_state(shift_out[i-1][127:0]), .o_result_state(sub_out[i-1][127:0]));
+        add_round_key add2(.i_state(sub_out[i-1][127:0]), .i_key(expanded_key[(4 * (NR + 1) * 32) - 1 -128*(NR - i) -:128]), .o_state(round_out[i-1][127:0]));
+        inv_mix_columns mix(.i_state(round_out[i-1][127:0]), .o_state(temp_state[i][127:0]));
+    end
+
+    wire [127:0]sub_out_f;
+    wire [127:0]shift_out_f;
+    wire [127:0]key_out_f;
+    inv_shift_rows shift(.i_state(temp_state[NR-1]), .o_state(shift_out_f));
+    inv_sub_bytes sub(.i_initial_state(shift_out_f), .o_result_state(sub_out_f));
+    add_round_key add2(.i_state(sub_out_f), .i_key(expanded_key[(4 * (NR + 1) * 32) - 1 -:128]), .o_state(key_out_f));
+
+endgenerate
+
+assign o_data = key_out_f;
+
+endmodule
---
+++ Decryption/tb_inv_cipher.v
@@ -0,0 +1,90 @@
+`include "inv_cipher.v"
+
+module tb_inv_cipher;
+
+reg [127:0] tb_state_0, tb_state_1, tb_state_2, tb_cipher_key_0;
+reg [191:0] tb_cipher_key_1;
+reg [255:0] tb_cipher_key_2;
+wire [127:0] result_0, result_1, result_2;
+
+inv_cipher #(4, 10) cipher_0 (
+    .i_data(tb_state_0),
+    .i_key(tb_cipher_key_0),
+    .o_data(result_0)
+);
+
+inv_cipher #(6, 12) cipher_1 (
+    .i_data(tb_state_1),
+    .i_key(tb_cipher_key_1),
+    .o_data(result_1)
+);
+
+inv_cipher #(8, 14) cipher_2 (
+    .i_data(tb_state_2),
+    .i_key(tb_cipher_key_2),
+    .o_data(result_2)
+);
+
+initial begin
+    tb_state_0 = 128'h69c4e0d86a7b0430d8cdb78070b4c55a;
+    tb_cipher_key_0 = 128'h000102030405060708090a0b0c0d0e0f;
+
+    tb_state_1 = 128'hdda97ca4864cdfe06eaf70a0ec0d7191;
+    tb_cipher_key_1 = 192'h000102030405060708090a0b0c0d0e0f1011121314151617;
+
+    tb_state_2 = 128'h8ea2b7ca516745bfeafc49904b496089;
+    tb_cipher_key_2 = 256'h000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f;
+
+    #100;
+    $display("Testcase 0");
+    $display("Input text:           %h", tb_state_0);
+    $display("Input key :           %h", tb_cipher_key_0);
+    $display("Expected Output: %h", 128'h00112233445566778899aabbccddeeff);
+    $display("Got:             %h", result_0);
+    if (result_0 != 128'h00112233445566778899aabbccddeeff) begin
+      $display("Testcase 0 FAILED");
+    end
+    else begin
+      $display("Testcase 0 PASSED");
+    end
+
+    $display("=================================================");
+
+    $display("Testcase 1");
+    $display("Input text:           %h", tb_state_1);
+    $display("Input key :           %h", tb_cipher_key_1);
+    $display("Expected Output: %h", 128'h00112233445566778899aabbccddeeff);
+    $display("Got:             %h", result_1);
+    if (result_1 != 128'h00112233445566778899aabbccddeeff) begin
+      $display("Testcase 1 FAILED");
+    end
+    else begin
+      $display("Testcase 1 PASSED");
+    end
+
+    $display("=================================================");
+
+    
+    $display("Testcase 2");
+    $display("Input text:           %h", tb_state_2);
+    $display("Input key :           %h", tb_cipher_key_2);
+    $display("Expected Output: %h", 128'h00112233445566778899aabbccddeeff);
+    $display("Got:             %h", result_2);
+    if (result_2 != 128'h00112233445566778899aabbccddeeff) begin
+      $display("Testcase 2 FAILED");
+    end
+    else begin
+      $display("Testcase 2 PASSED");
+    end
+
+    $display("=================================================");
+
+    #10;
+end
+
+initial begin
+  #300;
+  $finish;
+end
+
+endmodule
